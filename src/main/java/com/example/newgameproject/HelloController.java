26f58772f9720201a522a6e88ff9bcd1b2637c02
VitaliyKoshkin <kosh98vitaliy@outlook.com>
2022-12-12 15:40:51 +0300

    made an exit button

--- src/main/java/com/example/newgameproject/HelloController.java
+++ src/main/java/com/example/newgameproject/HelloController.java
@@ -1,14 +1,25 @@
 package com.example.newgameproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button closeButton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onExitButtonClick(ActionEvent actionEvent) {
+       Stage stage = (Stage) closeButton.getScene().getWindow();
+       stage.close();
     }
 }
